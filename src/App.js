d84a81e029b5047a6c41a6f7040ba4f0e46b7640
James Wong <jwong_li@hotmail.com>
2020-01-23 12:49:32 -0800

    implement a shopping list with delete functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import GroceryItem from './GroceryItem';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GROCERY_OBJECTS = []; // JS objects: item description, timestamp, etc...
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      groceries: [],
+      itemDescription: ''
+    };
+  }
+
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ itemDescription: value });
+    // console.log(e.target);
+  }
+
+  handleKeyPress = (e) => {
+    if(e.key === 'Enter'){
+      this.handleClickAddItem();
+    }
+  }
+
+  handleClickAddItem = () => {
+    if(this.state.itemDescription.trim() !== '') {
+      const object = { description: this.state.itemDescription, date: new Date() };
+      // console.log(object); // delete this line
+      GROCERY_OBJECTS.push(object);
+
+      this.setState({ 
+        groceries: GROCERY_OBJECTS,
+        itemDescription: ''
+      });
+    }    
+  }
+
+  handleClickRemoveItem = (item) => {
+    for(let i = 0; i < GROCERY_OBJECTS.length; i++) {
+      if(GROCERY_OBJECTS[i].description === item) {
+        GROCERY_OBJECTS.splice(i, 1);
+        this.setState({ groceries: GROCERY_OBJECTS });
+      }
+    }  
+    // console.log(GROCERY_OBJECTS); // delete this
+  }
+
+  render() {
+    const { groceries } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Shopping List</h1>
+        <div>
+          <input 
+            type="text"
+            value={this.state.itemDescription}
+            onChange={this.handleChange} 
+            onKeyPress={this.handleKeyPress}
+          />
+          <button onClick={this.handleClickAddItem}>Add to Shopping List</button>
+        </div>
+        <div id="items-container">
+          <ul>
+            {groceries.map((grocery) => 
+                <GroceryItem 
+                  key={grocery.description}
+                  item={grocery.description}
+                  date={grocery.date}
+                  onClick={this.handleClickRemoveItem}
+                />              
+            )}
+          </ul>
+        </div>
+      </div>
+    );
+  }  
 }
 
 export default App;
---
+++ src/GroceryItem.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class GroceryItem extends Component {
+    formatTimestamp = () => {
+        const { date } = this.props;
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        const minute = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes();
+        let time;
+
+        if (date.getHours() > 0 && date.getHours() < 12) {
+            time = `${date.getHours()}:${minute} AM`;
+
+        } else if (date.getHours() === 0){
+            time = `12:${minute} AM`;
+
+        } else if (date.getHours() === 12) {
+            time = `12:${minute} PM`;
+
+        } else {
+            time = `${date.getHours() - 12}:${minute} PM`;
+        }
+
+        const timestamp = `${months[date.getMonth()]} ${date.getDate()} ${time}`;
+        // console.log(timestamp);
+        // console.log('formatTimestamp(): ' + this.props.date.toString());
+        return timestamp;
+    }
+
+    render() {
+        return(
+            <li>
+                <div className="item-wrapper">
+                    <span>{this.props.item}</span>
+                    <div>
+                        <span className="time">{this.formatTimestamp()}</span>
+                        <button onClick={() => this.props.onClick(this.props.item)}>Delete</button>
+                    </div>                    
+                </div>                
+            </li>
+        );
+    }
+}
+
+export default GroceryItem;
