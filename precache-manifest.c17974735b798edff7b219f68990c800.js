7e58d06c337f41f21ebd5941f6762875164c2e90
James Wong <jwong_li@hotmail.com>
2020-02-01 15:13:19 -0800

    Updates

---
+++ precache-manifest.c17974735b798edff7b219f68990c800.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94407efdc3de7f9c2775b1e95d1ec73c",
+    "url": "/ShoppingList/index.html"
+  },
+  {
+    "revision": "2c3e44a2409b7278d363",
+    "url": "/ShoppingList/static/css/main.e4a1c1e2.chunk.css"
+  },
+  {
+    "revision": "5b6f7c34b49325757781",
+    "url": "/ShoppingList/static/js/2.cf62d31e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ShoppingList/static/js/2.cf62d31e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2c3e44a2409b7278d363",
+    "url": "/ShoppingList/static/js/main.6fa195db.chunk.js"
+  },
+  {
+    "revision": "cb2e1f193a2bd4a24113",
+    "url": "/ShoppingList/static/js/runtime-main.b5de3798.js"
+  }
+]);
